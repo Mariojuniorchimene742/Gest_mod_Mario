4d1dd3d04209507d696fe61537459139805a2bb6
Mariojuniorchimene <36141@esjaloures.org>
2025-12-01 15:57:15 +0000

    alteraçoes

---
+++ 3_gmm_turmas.sql
@@ -0,0 +1,17 @@
+USE grm_system;
+
+CREATE TABLE turmas (
+    id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    id_curso INT(11) UNSIGNED NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    ano_inicio YEAR NOT NULL,
+    ano_fim YEAR NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_curso) REFERENCES cursos(id)
+);
+
+INSERT INTO turmas (id_curso, nome, ano_inicio, ano_fim)
+VALUES 
+(1, '12ºTGPSI', '2023', '2026');
+
+SELECT * FROM turmas;
